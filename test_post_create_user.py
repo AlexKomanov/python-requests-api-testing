8d8a6bc548ca11cc6db7a8cefb5d03b109f1d356
AlexKomanov <akomanov88@gmail.com>
2024-05-24 14:23:35 +0300

    Adding POST lesson

---
+++ test_post_create_user.py
@@ -0,0 +1,28 @@
+import requests 
+from assertpy import assert_that
+
+def test_user_creation():
+    
+    name = "Alexander"
+    job = "QA Automation"
+    
+    name_entry = {"name": name}
+    job_entry = {"job": job}
+    
+    # creation_user_data = { **name_entry, **job_entry }
+    creation_user_data = name_entry|job_entry
+    
+    response = requests.post(
+        url="https://reqres.in/api/users",
+        json=creation_user_data
+    )
+    
+    assert_that(response.status_code).is_equal_to(201)
+    assert_that(response.ok).is_equal_to(True)
+    assert_that(response.reason).is_equal_to("Created")
+    json_response = response.json()
+    
+    assert_that(json_response["name"]).is_equal_to(name)
+    assert_that(json_response["job"]).is_equal_to(job)
+    assert_that(json_response).contains_entry(name_entry)
+    assert_that(json_response).contains_entry(job_entry)
---
+++ test_post_register_user.py
@@ -0,0 +1,35 @@
+import requests
+from assertpy import assert_that
+
+def test_register_user_hapy_flow():
+    
+    register_user_data = {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+        }
+    
+    response = requests.post(
+        url="https://reqres.in/api/register",
+        json=register_user_data
+        )
+    
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.ok).is_true()
+    assert_that(response.json()).contains_key("id")
+    assert "token" in response.json().keys()
+    
+
+def test_register_user_hapy_flow():
+    
+    register_user_data = {
+        "email": "eve.holt@reqres.in"
+        }
+    
+    response = requests.post(
+        url="https://reqres.in/api/register",
+        json=register_user_data
+        )
+    
+    assert_that(response.status_code).is_equal_to(400)
+    assert_that(response.ok).is_false()
+    assert_that(response.json()).contains_entry({"error": "Missing password"})
