a3bae9e9d487b9c4755d088b5beaf118348f2242
AlexKomanov <akomanov88@gmail.com>
2024-06-14 10:27:24 +0300

    Adding Update requests

---
+++ test_update_user.py
@@ -0,0 +1,48 @@
+import requests
+from assertpy import assert_that
+
+def test_update_user_with_put_request():
+        
+        name = "Alex"
+        
+        update_user_data = {
+        "name": name
+        }
+        
+        response = requests.put(
+            url="https://reqres.in/api/users/2",
+            json=update_user_data
+        )
+        
+        json_response = response.json()
+        
+        assert_that(response.status_code).is_equal_to(200)
+        assert_that(response.reason).is_equal_to("OK")
+        assert_that(json_response).contains_key("name")
+        assert_that(json_response["name"]).is_equal_to(name)
+        
+
+
+def test_update_user_with_patch_request():
+    name = "Alex"
+    job =  "QA Automation"
+        
+    update_user_data = {
+        "name": name,
+        "job": job
+        }
+        
+    response = requests.patch(
+            url="https://reqres.in/api/users/2",
+            json=update_user_data
+        )
+        
+    json_response = response.json()
+        
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.reason).is_equal_to("OK")
+    assert_that(json_response).contains_key("name")
+    assert_that(json_response).contains_key("job")
+    assert_that(json_response).contains_key("updatedAt")
+    assert_that(json_response["name"]).is_equal_to(name)
+    assert_that(json_response["job"]).is_equal_to(job)
---
+++ update_demonstration.py
@@ -0,0 +1,47 @@
+item = {
+    "name": "Alex",
+    "age": 36,
+    "address": "Karmiel",
+    "role": "QA Automation"
+}
+
+# PATCH REQUEST
+# item = {
+#     "name": "Alexander",
+#     "age": 37,
+# }
+
+# item = {
+#     "name": "Alexander",
+#     "age": 37,
+#     "address": "Karmiel",
+#     "role": "QA Automation"
+# }
+
+
+# PUT REQUEST
+# item = {
+#     "name": "Alexander",
+#     "age": 37,
+# }
+
+# item = {
+#     "name": "Alexander",
+#     "age": 37,
+# }
+
+
+# PUT REQUEST
+# item = {
+#     "name": "Alexander",
+#     "age": 37,
+#     "address": "Karmiel",
+#     "role": "QA Automation"
+# }
+
+# item = {
+#     "name": "Alexander",
+#     "age": 37,
+#     "address": "Karmiel",
+#     "role": "QA Automation"
+# }
